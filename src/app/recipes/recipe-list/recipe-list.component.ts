f4f2b407cbe71a3a71d9295fd1ba7250ab667bac
walid-hamdi <walidhamdidev@gmail.com>
2023-09-23 13:37:35 +0100

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      'Spaghetti Carbonara',
+      'A classic Italian pasta dish with eggs, cheese, pancetta, and black pepper.',
+      'https://images.unsplash.com/photo-1588013273468-315fd88ea34c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80'
+    ),
+    new Recipe(
+      'Chicken Alfredo',
+      'Creamy and cheesy pasta with grilled chicken breast and Alfredo sauce.',
+      'https://images.unsplash.com/photo-1670508142255-f119391c4213?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2031&q=80'
+    ),
+    new Recipe(
+      'Vegetable Stir-Fry',
+      'A healthy stir-fry with assorted vegetables and tofu in a savory sauce.',
+      'https://plus.unsplash.com/premium_photo-1664472637341-3ec829d1f4df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80'
+    ),
+  ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 4),
+    new Ingredient('Milk', 1),
+    new Ingredient('Bread', 1),
+    new Ingredient('Eggs', 12),
+  ];
+}
